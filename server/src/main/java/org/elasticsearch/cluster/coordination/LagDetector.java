de3ba5024bba58856f0ff102d649c3f31cfb3ffc
ghostspiders <15834128411@126.com>
2024-06-21 02:00:34 +0800

    raft 注释

--- server/src/main/java/org/elasticsearch/cluster/coordination/LagDetector.java
+++ server/src/main/java/org/elasticsearch/cluster/coordination/LagDetector.java
@@ -40,23 +40,31 @@ import java.util.stream.Collectors;
 import static org.elasticsearch.common.util.concurrent.ConcurrentCollections.newConcurrentMap;
 
 /**
- * A publication can succeed and complete before all nodes have applied the published state and acknowledged it; however we need every node
- * eventually either to apply the published state (or a later state) or be removed from the cluster. This component achieves this by
- * removing any lagging nodes from the cluster after a timeout.
+ * 一个发布操作可以在所有节点应用并确认发布状态之前成功并完成；然而，我们需要每个节点最终要么应用发布的这个状态（或更晚的状态），要么从集群中移除。
+ * 这个组件通过在超时后从集群中移除任何落后节点来实现这一点。
  */
 public class LagDetector {
 
     private static final Logger logger = LogManager.getLogger(LagDetector.class);
 
-    // the timeout for each node to apply a cluster state update after the leader has applied it, before being removed from the cluster
+    // 定义集群状态更新超时设置，这是每个节点在领导者应用更新后，应用更新之前的时间限制，如果超时则可能从集群中移除该节点
     public static final Setting<TimeValue> CLUSTER_FOLLOWER_LAG_TIMEOUT_SETTING =
-        Setting.timeSetting("cluster.follower_lag.timeout",
-            TimeValue.timeValueMillis(90000), TimeValue.timeValueMillis(1), Setting.Property.NodeScope);
+        Setting.timeSetting(
+            "cluster.follower_lag.timeout", // 设置的键名
+            TimeValue.timeValueMillis(90000), // 默认值，90秒
+            TimeValue.timeValueMillis(1), // 最小值，至少1毫秒
+            Setting.Property.NodeScope // 作用域为节点级别
+        );
 
+    // 集群状态应用超时时间
     private final TimeValue clusterStateApplicationTimeout;
+    // 当检测到节点落后时调用的消费者函数
     private final Consumer<DiscoveryNode> onLagDetected;
+    // 提供本地节点信息的供应者函数
     private final Supplier<DiscoveryNode> localNodeSupplier;
+    // 线程池，用于异步操作
     private final ThreadPool threadPool;
+    // 存储每个节点及其对应的已应用状态追踪器的映射，使用线程安全的并发映射
     private final Map<DiscoveryNode, NodeAppliedStateTracker> appliedStateTrackersByNode = newConcurrentMap();
 
     public LagDetector(final Settings settings, final ThreadPool threadPool, final Consumer<DiscoveryNode> onLagDetected,
@@ -67,45 +75,73 @@ public class LagDetector {
         this.localNodeSupplier = localNodeSupplier;
     }
 
+    /**
+     * 设置要追踪的节点集合。
+     * @param discoveryNodes 要设置的节点集合。
+     */
     public void setTrackedNodes(final Iterable<DiscoveryNode> discoveryNodes) {
         final Set<DiscoveryNode> discoveryNodeSet = new HashSet<>();
+        // 将Iterable转换为Set。
         discoveryNodes.forEach(discoveryNodeSet::add);
+        // 从集合中移除本地节点。
         discoveryNodeSet.remove(localNodeSupplier.get());
+        // 保留在新集合中的节点对应的追踪器，移除其他节点的追踪器。
         appliedStateTrackersByNode.keySet().retainAll(discoveryNodeSet);
+        // 为新集合中的每个节点创建追踪器，如果尚未创建。
         discoveryNodeSet.forEach(node -> appliedStateTrackersByNode.putIfAbsent(node, new NodeAppliedStateTracker(node)));
     }
 
+    /**
+     * 清除所有追踪的节点。
+     */
     public void clearTrackedNodes() {
+        // 清除所有节点状态追踪器。
         appliedStateTrackersByNode.clear();
     }
 
+    /**
+     * 设置指定节点已应用的版本。
+     * @param discoveryNode 指定的节点。
+     * @param appliedVersion 已应用的版本号。
+     */
     public void setAppliedVersion(final DiscoveryNode discoveryNode, final long appliedVersion) {
         final NodeAppliedStateTracker nodeAppliedStateTracker = appliedStateTrackersByNode.get(discoveryNode);
         if (nodeAppliedStateTracker == null) {
-            // Received an ack from a node that a later publication has removed (or we are no longer master). No big deal.
+            // 如果收到已移除节点的确认信息，或者当前不再是主节点，则无需追踪该节点的版本。
             logger.trace("node {} applied version {} but this node's version is not being tracked", discoveryNode, appliedVersion);
         } else {
+            // 增加节点已应用的版本。
             nodeAppliedStateTracker.increaseAppliedVersion(appliedVersion);
         }
     }
 
+    /**
+     * 开始检测落后的节点。
+     * @param version 要检测的版本号。
+     */
     public void startLagDetector(final long version) {
+        // 筛选出应用版本小于指定版本的节点状态追踪器。
         final List<NodeAppliedStateTracker> laggingTrackers
             = appliedStateTrackersByNode.values().stream().filter(t -> t.appliedVersionLessThan(version)).collect(Collectors.toList());
 
         if (laggingTrackers.isEmpty()) {
+            // 如果没有落后的追踪器，则无需进行延迟检测。
             logger.trace("lag detection for version {} is unnecessary: {}", version, appliedStateTrackersByNode.values());
         } else {
+            // 如果存在落后的追踪器，记录日志并启动延迟检测。
             logger.debug("starting lag detector for version {}: {}", version, laggingTrackers);
 
+            // 根据设置的超时时间，在线程池中调度延迟检测任务。
             threadPool.scheduleUnlessShuttingDown(clusterStateApplicationTimeout, Names.GENERIC, new Runnable() {
                 @Override
                 public void run() {
+                    // 对落后的追踪器进行检查。
                     laggingTrackers.forEach(t -> t.checkForLag(version));
                 }
 
                 @Override
                 public String toString() {
+                    // 提供任务的字符串表示，用于日志记录。
                     return "lag detector for version " + version + " on " + laggingTrackers;
                 }
             });
--- server/src/main/java/org/elasticsearch/cluster/coordination/Reconfigurator.java
+++ server/src/main/java/org/elasticsearch/cluster/coordination/Reconfigurator.java
@@ -37,41 +37,61 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * Computes the optimal configuration of voting nodes in the cluster.
+ * 计算集群中投票节点的最佳配置
  */
 public class Reconfigurator {
 
     private static final Logger logger = LogManager.getLogger(Reconfigurator.class);
 
     /**
-     * The cluster usually requires a vote from at least half of the master nodes in order to commit a cluster state update, and to achieve
-     * the best resilience it makes automatic adjustments to the voting configuration as master nodes join or leave the cluster. Adjustments
-     * that fix or increase the size of the voting configuration are always a good idea, but the wisdom of reducing the voting configuration
-     * size is less clear. For instance, automatically reducing the voting configuration down to a single node means the cluster requires
-     * this node to operate, which is not resilient: if it broke we could restore every other master-eligible node in the cluster to health
-     * and still the cluster would be unavailable. However not reducing the voting configuration size can also hamper resilience: in a
-     * five-node cluster we could lose two nodes and by reducing the voting configuration to the remaining three nodes we could tolerate the
-     * loss of a further node before failing.
-     *
-     * We offer two options: either we auto-shrink the voting configuration as long as it contains more than three nodes, or we don't and we
-     * require the user to control the voting configuration manually using the retirement API. The former, default, option, guarantees that
-     * as long as there have been at least three master-eligible nodes in the cluster and no more than one of them is currently unavailable,
-     * then the cluster will still operate, which is what almost everyone wants. Manual control is for users who want different guarantees.
+     投票配置的重要性：集群需要至少一半的主节点投票同意才能更新状态。如果自动调整投票配置，可以提高集群的容错性。
+
+     调整投票配置的考虑：增加投票配置的大小是有益的，因为它可以提高集群在面对节点故障时的容错能力。然而，减少投票配置的大小可能不那么明智，因为它会降低容错性。例如，如果自动减少到只有一个节点，那么这个节点的故障将导致整个集群不可用。
+
+     自动调整的策略：有两种策略可供选择：
+
+     自动缩减：只要投票配置包含超过三个节点，就自动缩减投票配置。这种默认选项确保了只要集群中至少有三个主节点，并且其中不超过一个节点不可用，集群就能继续运行。
+     手动控制：不自动缩减投票配置，而是要求用户使用退休API手动控制投票配置。这种方法适用于需要不同保证的用户。
+     容错性的权衡：在五节点集群中，如果失去两个节点，通过将投票配置减少到剩下的三个节点，我们可以容忍进一步失去一个节点而不会导致集群失败。但是，如果不减少投票配置的大小，也可能影响集群的容错性。
+     */
+    /**
+     * 定义了一个设置项，用于控制是否自动缩减集群的投票配置。
+     * 默认值为true，表示开启自动缩减功能。这个设置项可以在节点级别动态更新。
      */
     public static final Setting<Boolean> CLUSTER_AUTO_SHRINK_VOTING_CONFIGURATION =
         Setting.boolSetting("cluster.auto_shrink_voting_configuration", true, Property.NodeScope, Property.Dynamic);
 
+    /**
+     * 用于存储自动缩减投票配置的状态，标记为volatile以确保多线程环境下的可见性。
+     */
     private volatile boolean autoShrinkVotingConfiguration;
 
+    /**
+     * Reconfigurator类的构造函数。
+     * @param settings 包含集群设置的Settings对象。
+     * @param clusterSettings 用于管理集群设置的ClusterSettings对象。
+     */
     public Reconfigurator(Settings settings, ClusterSettings clusterSettings) {
+        // 从settings中获取自动缩减投票配置的状态，并存储。
         autoShrinkVotingConfiguration = CLUSTER_AUTO_SHRINK_VOTING_CONFIGURATION.get(settings);
+        // 注册设置更新的消费者，当CLUSTER_AUTO_SHRINK_VOTING_CONFIGURATION设置更新时，调用setAutoShrinkVotingConfiguration方法。
         clusterSettings.addSettingsUpdateConsumer(CLUSTER_AUTO_SHRINK_VOTING_CONFIGURATION, this::setAutoShrinkVotingConfiguration);
     }
 
+    /**
+     * 更新自动缩减投票配置的状态。
+     * @param autoShrinkVotingConfiguration 新的自动缩减状态。
+     */
     public void setAutoShrinkVotingConfiguration(boolean autoShrinkVotingConfiguration) {
         this.autoShrinkVotingConfiguration = autoShrinkVotingConfiguration;
     }
 
+    /**
+     * 将输入的整数大小向下取到最近的奇数。
+     * 如果输入是偶数，则减去1；如果是奇数，则保持不变。
+     * @param size 输入的整数大小。
+     * @return 向下取到最近的奇数值。
+     */
     private static int roundDownToOdd(int size) {
         return size - (size % 2 == 0 ? 1 : 0);
     }
@@ -84,53 +104,59 @@ public class Reconfigurator {
     }
 
     /**
-     * Compute an optimal configuration for the cluster.
+     * 计算集群的最优配置。
      *
-     * @param liveNodes      The live nodes in the cluster. The optimal configuration prefers live nodes over non-live nodes as far as
-     *                       possible.
-     * @param retiredNodeIds Nodes that are leaving the cluster and which should not appear in the configuration if possible. Nodes that are
-     *                       retired and not in the current configuration will never appear in the resulting configuration; this is useful
-     *                       for shifting the vote in a 2-node cluster so one of the nodes can be restarted without harming availability.
-     * @param currentMaster  The current master. Unless retired, we prefer to keep the current master in the config.
-     * @param currentConfig  The current configuration. As far as possible, we prefer to keep the current config as-is.
-     * @return An optimal configuration, or leave the current configuration unchanged if the optimal configuration has no live quorum.
+     * @param liveNodes              集群中活跃的节点。最优配置尽可能优先选择活跃节点。
+     * @param retiredNodeIds         即将离开集群的节点，如果可能的话，这些节点不应出现在配置中。如果已经退休的节点不在当前配置中，
+     *                              那么它们也不会出现在结果配置中；这在两节点集群中非常有用，可以重新启动其中一个节点而不会影响可用性。
+     * @param currentMaster          当前的主节点。除非已退休，我们更倾向于在配置中保留当前主节点。
+     * @param currentConfig          当前的配置。尽可能保持当前配置不变。
+     * @return 一个最优配置，或者如果最优配置没有活跃的法定人数，则保持当前配置不变。
      */
     public VotingConfiguration reconfigure(Set<DiscoveryNode> liveNodes, Set<String> retiredNodeIds, DiscoveryNode currentMaster,
                                            VotingConfiguration currentConfig) {
+        // 断言检查，确保活跃节点不是Zen1节点，并且当前主节点包含在活跃节点中。
         assert liveNodes.stream().noneMatch(Coordinator::isZen1Node) : liveNodes;
         assert liveNodes.contains(currentMaster) : "liveNodes = " + liveNodes + " master = " + currentMaster;
+        // 日志记录当前配置的重新配置过程。
         logger.trace("{} reconfiguring {} based on liveNodes={}, retiredNodeIds={}, currentMaster={}",
             this, currentConfig, liveNodes, retiredNodeIds, currentMaster);
 
         /*
-         *  There are three true/false properties of each node in play: live/non-live, retired/non-retired and in-config/not-in-config.
-         *  Firstly we divide the nodes into disjoint sets based on these properties:
+         * 每个节点有三种属性：活跃/非活跃、退休/非退休和在配置中/不在配置中。
+         * 首先，我们根据这些属性将节点划分为不同的集合：
          *
-         *  - nonRetiredMaster
-         *  - nonRetiredNotMasterInConfigNotLiveIds
-         *  - nonRetiredInConfigLiveIds
-         *  - nonRetiredLiveNotInConfigIds
+         * - 非退休主节点
+         * - 非退休不在配置中且非活跃节点ID
+         * - 非退休在配置中且活跃节点ID
+         * - 非退休不在配置中且活跃节点ID
          *
-         *  The other 5 possibilities are not relevant:
-         *  - retired, in-config, live             -- retired nodes should be removed from the config
-         *  - retired, in-config, non-live         -- retired nodes should be removed from the config
-         *  - retired, not-in-config, live         -- cannot add a retired node back to the config
-         *  - retired, not-in-config, non-live     -- cannot add a retired node back to the config
-         *  - non-retired, non-live, not-in-config -- no evidence this node exists at all
+         * 其他5种可能性不相关：
+         * - 已退休，在配置中，活跃           -- 应从配置中移除退休节点
+         * - 已退休，在配置中，非活跃         -- 同上
+         * - 已退休，不在配置中，活跃         -- 不能将退休节点重新添加到配置中
+         * - 已退休，不在配置中，非活跃       -- 同上
+         * - 非退休，非活跃，不在配置中       -- 没有证据表明这个节点存在
          */
 
+        // 计算活跃节点的ID集合。
         final Set<String> liveNodeIds = liveNodes.stream()
             .filter(DiscoveryNode::isMasterNode).map(DiscoveryNode::getId).collect(Collectors.toSet());
+        // 从当前配置中获取活跃节点的ID集合。
         final Set<String> liveInConfigIds = new TreeSet<>(currentConfig.getNodeIds());
         liveInConfigIds.retainAll(liveNodeIds);
 
+        // 计算在配置中但不活跃的节点ID集合。
         final Set<String> inConfigNotLiveIds = Sets.sortedDifference(currentConfig.getNodeIds(), liveInConfigIds);
+        // 计算非退休、在配置中但不活跃的节点ID集合。
         final Set<String> nonRetiredInConfigNotLiveIds = new TreeSet<>(inConfigNotLiveIds);
         nonRetiredInConfigNotLiveIds.removeAll(retiredNodeIds);
 
+        // 计算非退休、在配置中且活跃的节点ID集合。
         final Set<String> nonRetiredInConfigLiveIds = new TreeSet<>(liveInConfigIds);
         nonRetiredInConfigLiveIds.removeAll(retiredNodeIds);
 
+        // 根据当前主节点ID，进一步划分配置中活跃节点为两类：主节点和其他活跃节点。
         final Set<String> nonRetiredInConfigLiveMasterIds;
         final Set<String> nonRetiredInConfigLiveNotMasterIds;
         if (nonRetiredInConfigLiveIds.contains(currentMaster.getId())) {
@@ -142,41 +168,41 @@ public class Reconfigurator {
             nonRetiredInConfigLiveMasterIds = Collections.emptySet();
         }
 
+        // 计算非退休、不在配置中且活跃的节点ID集合。
         final Set<String> nonRetiredLiveNotInConfigIds = Sets.sortedDifference(liveNodeIds, currentConfig.getNodeIds());
         nonRetiredLiveNotInConfigIds.removeAll(retiredNodeIds);
 
         /*
-         * Now we work out how many nodes should be in the configuration:
+         * 现在我们计算配置中应该有多少节点：
          */
         final int targetSize;
 
+        // 计算非退休活跃节点的总数。
         final int nonRetiredLiveNodeCount = nonRetiredInConfigLiveIds.size() + nonRetiredLiveNotInConfigIds.size();
+        // 计算非退休配置中节点的总数。
         final int nonRetiredConfigSize = nonRetiredInConfigLiveIds.size() + nonRetiredInConfigNotLiveIds.size();
         if (autoShrinkVotingConfiguration) {
             if (nonRetiredLiveNodeCount >= 3) {
+                // 如果非退休活跃节点数大于等于3，则目标大小为非退休活跃节点数向下取到最近的奇数。
                 targetSize = roundDownToOdd(nonRetiredLiveNodeCount);
             } else {
-                // only have one or two available nodes; may not shrink below 3 nodes automatically, but if
-                // the config (excluding retired nodes) is already smaller than 3 then it's ok.
+                // 如果只有一到两个可用节点，可能不会自动缩减到3个节点以下，但如果配置（不包括退休节点）已经小于3，则可以。
                 targetSize = nonRetiredConfigSize < 3 ? 1 : 3;
             }
         } else {
+            // 如果不自动缩减，目标大小为非退休活跃节点数向下取到最近的奇数和非退休配置大小的较大者。
             targetSize = Math.max(roundDownToOdd(nonRetiredLiveNodeCount), nonRetiredConfigSize);
         }
 
         /*
-         * The new configuration is formed by taking this many nodes in the following preference order:
+         * 形成新的配置，按照以下优先顺序选择节点：
          */
         final VotingConfiguration newConfig = new VotingConfiguration(
-            // live master first, then other live nodes, preferring the current config, and if we need more then use non-live nodes
+            // 首先选择活跃的主节点，然后是其他活跃节点，优先考虑当前配置，如果需要更多节点，则使用非活跃节点。
             Stream.of(nonRetiredInConfigLiveMasterIds, nonRetiredInConfigLiveNotMasterIds, nonRetiredLiveNotInConfigIds,
-                nonRetiredInConfigNotLiveIds).flatMap(Collection::stream).limit(targetSize).collect(Collectors.toSet()));
+                nonRetiredInConfigNotLiveIds).flatMap(Collection::stream).limit(targetSize).collect(Collectors.toSet())));
 
-        if (newConfig.hasQuorum(liveNodeIds)) {
-            return newConfig;
-        } else {
-            // If there are not enough live nodes to form a quorum in the newly-proposed configuration, it's better to do nothing.
-            return currentConfig;
-        }
+        // 如果新配置在活跃节点中有法定人数，则返回新配置；否则，如果新提出的配置中没有足够的活跃节点形成法定人数，最好什么都不做。
+        return newConfig.hasQuorum(liveNodeIds) ? newConfig : currentConfig;
     }
 }
