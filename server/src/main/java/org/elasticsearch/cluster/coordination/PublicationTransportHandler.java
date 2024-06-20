8dbaeeb34050fabda499fe7fffd5188d0824e00f
ghostspiders <yufeng.gao@seres.cn>
2024-06-20 18:20:57 +0800

    raft

--- server/src/main/java/org/elasticsearch/cluster/coordination/PublicationTransportHandler.java
+++ server/src/main/java/org/elasticsearch/cluster/coordination/PublicationTransportHandler.java
@@ -64,22 +64,54 @@ import java.util.function.Function;
 
 public class PublicationTransportHandler {
 
+    /**
+     * 日志记录器。
+     */
     private static final Logger logger = LogManager.getLogger(PublicationTransportHandler.class);
 
+    /**
+     * 发布状态操作的名称。
+     */
     public static final String PUBLISH_STATE_ACTION_NAME = "internal:cluster/coordination/publish_state";
+
+    /**
+     * 提交状态操作的名称。
+     */
     public static final String COMMIT_STATE_ACTION_NAME = "internal:cluster/coordination/commit_state";
 
+    // 传输服务，用于节点间的通信。
     private final TransportService transportService;
+
+    // 命名写入注册表，用于序列化和反序列化。
     private final NamedWriteableRegistry namedWriteableRegistry;
+
+    // 处理发布请求的函数。
     private final Function<PublishRequest, PublishWithJoinResponse> handlePublishRequest;
 
+    /**
+     * 最后观察到的集群状态的原子引用。
+     */
     private AtomicReference<ClusterState> lastSeenClusterState = new AtomicReference<>();
 
+    /**
+     * 接收到的完整集群状态计数器。
+     */
     private final AtomicLong fullClusterStateReceivedCount = new AtomicLong();
+
+    /**
+     * 接收到的不兼容集群状态差异计数器。
+     */
     private final AtomicLong incompatibleClusterStateDiffReceivedCount = new AtomicLong();
+
+    /**
+     * 接收到的兼容集群状态差异计数器。
+     */
     private final AtomicLong compatibleClusterStateDiffReceivedCount = new AtomicLong();
-    // -> no need to put a timeout on the options here, because we want the response to eventually be received
-    //  and not log an error if it arrives after the timeout
+
+    /**
+     * 状态请求的传输选项。
+     * 这里没有设置超时，因为我们希望响应最终能被接收到，即使它在超时后到达，也不要记录错误。
+     */
     private final TransportRequestOptions stateRequestOptions = TransportRequestOptions.builder()
         .withType(TransportRequestOptions.Type.STATE).build();
 
@@ -161,62 +193,73 @@ public class PublicationTransportHandler {
 
     }
 
+    /**
+     * 创建一个新的发布上下文。
+     * @param clusterChangedEvent 集群变更事件。
+     * @return 返回一个新的PublicationContext实例。
+     */
     public PublicationContext newPublicationContext(ClusterChangedEvent clusterChangedEvent) {
-        final DiscoveryNodes nodes = clusterChangedEvent.state().nodes();
-        final ClusterState newState = clusterChangedEvent.state();
-        final ClusterState previousState = clusterChangedEvent.previousState();
-        final boolean sendFullVersion = clusterChangedEvent.previousState().getBlocks().disableStatePersistence();
-        final Map<Version, BytesReference> serializedStates = new HashMap<>();
-        final Map<Version, BytesReference> serializedDiffs = new HashMap<>();
+        final DiscoveryNodes nodes = clusterChangedEvent.state().nodes(); // 获取集群节点信息
+        final ClusterState newState = clusterChangedEvent.state(); // 获取新状态
+        final ClusterState previousState = clusterChangedEvent.previousState(); // 获取之前的状态
+        final boolean sendFullVersion = clusterChangedEvent.previousState().getBlocks().disableStatePersistence(); // 是否发送完整版本
+        final Map<Version, BytesReference> serializedStates = new HashMap<>(); // 序列化状态的映射
+        final Map<Version, BytesReference> serializedDiffs = new HashMap<>(); // 序列化差异的映射
 
-        // we build these early as a best effort not to commit in the case of error.
-        // sadly this is not water tight as it may that a failed diff based publishing to a node
-        // will cause a full serialization based on an older version, which may fail after the
-        // change has been committed.
+        // 尽早构建这些，作为在错误情况下不提交的最佳努力。
+        // 遗憾的是，这并不完美，因为基于差异的发布失败可能会导致基于旧版本的完整序列化，
+        // 这可能在变更已提交后失败。
         buildDiffAndSerializeStates(clusterChangedEvent.state(), clusterChangedEvent.previousState(),
             nodes, sendFullVersion, serializedStates, serializedDiffs);
 
+        // 定义一个新的发布上下文实现
         return new PublicationContext() {
+            // 覆盖sendPublishRequest方法以发送发布请求
             @Override
             public void sendPublishRequest(DiscoveryNode destination, PublishRequest publishRequest,
                                            ActionListener<PublishWithJoinResponse> responseActionListener) {
+                // 断言传入的发布请求状态与集群变更事件状态一致
                 assert publishRequest.getAcceptedState() == clusterChangedEvent.state() : "state got switched on us";
+
+                // 如果目标节点是本地节点，则在本地执行发布请求处理
                 if (destination.equals(nodes.getLocalNode())) {
-                    // the master needs the original non-serialized state as the cluster state contains some volatile information that we
-                    // don't want to be replicated because it's not usable on another node (e.g. UnassignedInfo.unassignedTimeNanos) or
-                    // because it's mostly just debugging info that would unnecessarily blow up CS updates (I think there was one in
-                    // snapshot code).
-                    // TODO: look into these and check how to get rid of them
+                    // 执行发布请求处理并响应
                     transportService.getThreadPool().generic().execute(new AbstractRunnable() {
+                        // 覆盖onFailure方法以处理可能的异常
                         @Override
                         public void onFailure(Exception e) {
-                            // wrap into fake TransportException, as that's what we expect in Publication
                             responseActionListener.onFailure(new TransportException(e));
                         }
 
+                        // 覆盖doRun方法以执行发布请求处理
                         @Override
                         protected void doRun() {
                             responseActionListener.onResponse(handlePublishRequest.apply(publishRequest));
                         }
 
+                        // 覆盖toString方法以提供Runnable的字符串表示
                         @Override
                         public String toString() {
                             return "publish to self of " + publishRequest;
                         }
                     });
                 } else if (sendFullVersion || !previousState.nodes().nodeExists(destination)) {
+                    // 如果需要发送完整版本或目标节点在之前状态中不存在，则发送完整集群状态
                     logger.trace("sending full cluster state version {} to {}", newState.version(), destination);
                     PublicationTransportHandler.this.sendFullClusterState(newState, serializedStates, destination, responseActionListener);
                 } else {
+                    // 否则发送集群状态差异
                     logger.trace("sending cluster state diff for version {} to {}", newState.version(), destination);
                     PublicationTransportHandler.this.sendClusterStateDiff(newState, serializedDiffs, serializedStates, destination,
                         responseActionListener);
                 }
             }
 
+            // 覆盖sendApplyCommit方法以发送应用提交请求
             @Override
             public void sendApplyCommit(DiscoveryNode destination, ApplyCommitRequest applyCommitRequest,
                                         ActionListener<TransportResponse.Empty> responseActionListener) {
+                // 根据目标节点是否是Zen1节点选择操作名称和传输请求
                 final String actionName;
                 final TransportRequest transportRequest;
                 if (Coordinator.isZen1Node(destination)) {
@@ -226,24 +269,28 @@ public class PublicationTransportHandler {
                     actionName = COMMIT_STATE_ACTION_NAME;
                     transportRequest = applyCommitRequest;
                 }
+                // 发送应用提交请求
                 transportService.sendRequest(destination, actionName, transportRequest, stateRequestOptions,
                     new TransportResponseHandler<TransportResponse.Empty>() {
-
+                        // 覆盖read方法以读取响应
                         @Override
                         public TransportResponse.Empty read(StreamInput in) {
                             return TransportResponse.Empty.INSTANCE;
                         }
 
+                        // 覆盖handleResponse方法以处理响应
                         @Override
                         public void handleResponse(TransportResponse.Empty response) {
                             responseActionListener.onResponse(response);
                         }
 
+                        // 覆盖handleException方法以处理异常
                         @Override
                         public void handleException(TransportException exp) {
                             responseActionListener.onFailure(exp);
                         }
 
+                        // 覆盖executor方法以指定响应处理器的线程池
                         @Override
                         public String executor() {
                             return ThreadPool.Names.GENERIC;
